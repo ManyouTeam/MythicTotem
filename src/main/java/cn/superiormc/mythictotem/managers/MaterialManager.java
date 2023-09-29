f60e707bf6c0683c6b41f48c905e9cde48eb3ee1
PQguanfang <1686965528@qq.com>
2023-09-29 20:55:19 +0800

    2.11.0

--- src/main/java/cn/superiormc/mythictotem/managers/MaterialManager.java
+++ src/main/java/cn/superiormc/mythictotem/managers/MaterialManager.java
@@ -1,9 +1,14 @@
 package cn.superiormc.mythictotem.managers;
 
+import cn.superiormc.mythictotem.MythicTotem;
 import dev.lone.itemsadder.api.CustomBlock;
 import io.th0rgal.oraxen.api.OraxenBlocks;
+import net.Indyuce.mmoitems.MMOItems;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+
+import java.util.Optional;
 
 public class MaterialManager {
 
@@ -16,22 +21,22 @@ public class MaterialManager {
         this.block = block;
     }
     public boolean CheckMaterial(){
+        if (MythicTotem.instance.getConfig().getBoolean("settings.debug")) {
+            Bukkit.getConsoleSender().sendMessage("§x§9§8§F§B§9§8[MythicTotem] §aReal block: " + block + ".");
+        }
         if (material.equals("none")) {
             return true;
-        }
-        else if (material.startsWith("minecraft:")) {
+        } else if (material.startsWith("minecraft:")) {
             try {
                 return (Material.valueOf(material.split(":")[1].toUpperCase()) == block.getType());
             } catch (IllegalArgumentException | NullPointerException ignored) {
             }
-        }
-        else if (material.startsWith("itemsadder:")) {
+        } else if (material.startsWith("itemsadder:")) {
             try {
                 return (material.split(":")[1] + ":" + material.split(":")[2]).equals(CustomBlock.byAlreadyPlaced(block).getNamespacedID());
             } catch (NullPointerException ignored) {
             }
-        }
-        else if (material.startsWith("oraxen:")) {
+        } else if (material.startsWith("oraxen:")) {
             try {
                 if (OraxenBlocks.getNoteBlockMechanic(block).getItemID() == null){
                     return (material.split(":")[1]).equals(OraxenBlocks.getStringMechanic(block).getItemID());
@@ -41,6 +46,10 @@ public class MaterialManager {
                 }
             } catch (NullPointerException ignored) {
             }
+        } else if (material.startsWith("mmoitems:")) {
+            Optional<net.Indyuce.mmoitems.api.block.CustomBlock> opt = MMOItems.plugin.getCustomBlocks().
+                    getFromBlock(block.getBlockData());
+            return opt.filter(customBlock -> customBlock.getId() == Integer.parseInt(material.split(":")[1])).isPresent();
         } else {
             try {
                 return (Material.valueOf(material.split(":")[1].toUpperCase()) == block.getType());
