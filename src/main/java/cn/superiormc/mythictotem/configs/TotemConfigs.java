ac71a9886fc339c88f3cbda2526d0dcfb2dd6d94
PQguanfang <pqguanfang@foxmail.com>
2024-05-11 12:51:22 +0800

    [3.5.8] Support subfolder load in totem configs

--- src/main/java/cn/superiormc/mythictotem/configs/TotemConfigs.java
+++ src/main/java/cn/superiormc/mythictotem/configs/TotemConfigs.java
@@ -2,9 +2,6 @@ package cn.superiormc.mythictotem.configs;
 
 import cn.superiormc.mythictotem.MythicTotem;
 import cn.superiormc.mythictotem.managers.TotemManager;
-import io.lumine.xikage.mythicmobs.MythicMobs;
-import org.bukkit.Bukkit;
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
@@ -12,21 +9,29 @@ import java.util.*;
 
 public class TotemConfigs {
 
-    public static void initTotemConfigs(){
-       File dir = new File(MythicTotem.instance.getDataFolder(), "totems");
-       if (!dir.exists()) {
-           dir.mkdir();
-       }
-       File[] files = dir.listFiles();
-       if (!Objects.nonNull(files) && files.length != 0) {
-           return;
-       }
-       for (File file : files) {
-           String fileName = file.getName();
-           if (fileName.endsWith(".yml")) {
-               String substring = fileName.substring(0, fileName.length() - 4);
-               MythicTotem.getTotemMap.put(substring, new TotemManager(substring, YamlConfiguration.loadConfiguration(file)));
-           }
-       }
+    public static void initTotemConfigs() {
+        File dir = new File(MythicTotem.instance.getDataFolder(), "totems");
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
+        loadTotems(dir);
+    }
+
+    private static void loadTotems(File folder) {
+        File[] files = folder.listFiles();
+        if (!Objects.nonNull(files)) {
+            return;
+        }
+        for (File file : files) {
+            if (file.isDirectory()) {
+                loadTotems(file); // Recursive call to load files from subfolders
+            } else {
+                String fileName = file.getName();
+                if (fileName.endsWith(".yml")) {
+                    String substring = fileName.substring(0, fileName.length() - 4);
+                    MythicTotem.getTotemMap.put(substring, new TotemManager(substring, YamlConfiguration.loadConfiguration(file)));
+                }
+            }
+        }
     }
 }
